0a3a27f7d588f0163b761afe5fc8a3992ce82fc6
kenanbylan <kenan.baylan4654@gmail.com>
2023-04-06 14:04:37 +0300

    Initial Commit

---
+++ OrderFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderFood
+//
+//  Created by Kenan Baylan on 6.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
